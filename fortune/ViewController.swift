fe5cf9fe0ecd4416bc3dc0f7b6e56a1879b33234
“iwataTMC” <abcwwwxyz888@gmail.com>
2021-11-30 09:45:38 +0900

    Edit images

--- fortune/ViewController.swift
+++ fortune/ViewController.swift
@@ -24,9 +24,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerLabel: UILabel!
     
     
-    
+    var answerNumber = 0
     
     @IBAction func shuffleAction(_ sender: Any) {
+
+
+        // 0から7未満
+        answerNumber = Int.random(in: 0..<7)
+        
+        if answerNumber == 0 {
+            answerLabel.text = "大凶..."
+            answerImageView.image = UIImage(named: "omikuji-daikyo")
+        } else if answerNumber == 1 {
+            answerLabel.text = "大吉！！！！"
+            answerImageView.image = UIImage(named: "omikuji-daikichi")
+        } else if answerNumber == 2 {
+            answerLabel.text = "凶...！"
+            answerImageView.image = UIImage(named: "omikuji-kyo")
+        } else if answerNumber == 3 {
+            answerLabel.text = "中吉...！"
+            answerImageView.image = UIImage(named: "omikuji-chukichi")
+        } else if answerNumber == 4 {
+            answerLabel.text = "半凶...！"
+            answerImageView.image = UIImage(named: "omikuji-hankyo")
+        } else if answerNumber == 5 {
+            answerLabel.text = "小凶...！"
+            answerImageView.image = UIImage(named: "omikuji-shokyo")
+        } else if answerNumber == 6 {
+            answerLabel.text = "末凶...！"
+            answerImageView.image = UIImage(named: "omikuji-suekyo")
+        }
+        
+        
+        
     }
     
     
