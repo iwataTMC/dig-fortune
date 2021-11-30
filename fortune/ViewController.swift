24480ab2a6b5df7bf0d1cd41cbc44fede1d13bf9
“iwataTMC” <abcwwwxyz888@gmail.com>
2021-11-30 09:13:34 +0900

    add image

--- fortune/ViewController.swift
+++ fortune/ViewController.swift
@@ -12,8 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        answerLabel.text = "Today is ...??"
+        answerImageView.image = UIImage(named: "omikuji")
     }
 
-
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
+    
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+    }
+    
+    
+    
+    
 }
 
